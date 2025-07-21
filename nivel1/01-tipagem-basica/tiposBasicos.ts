57aaefbc06df7ef7b1b313d7b02b13c012b450df
Bruno Waschburger Silva <bwaschburger@gmail.com>
2025-07-21 16:53:47 -0300

    Exercício 01 tipagem básica  resolvido

---
+++ nivel1/01-tipagem-basica/tiposBasicos.ts
@@ -0,0 +1,12 @@
+let nome: String = "Bruno";
+let idade: number = 17;
+let ativo: boolean = true;
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Usuário ativo? " + ativo);
+
+let hobbies: String[] = ["programar, jogar, desenvolver sites"];
+console.log ("Hobbies do usuário: " + hobbies);
+
+let pontosXY: [number, number] = [2, 5];
+console.log("Plano cartesiano: " + pontosXY);
